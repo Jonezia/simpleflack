36fdab2016b69b142026feca5d7356748dfb5b88
Jonezia <adamj6636@gmail.com>
2018-09-03 23:41:17 +0800

    Channel creation with socketio

--- app/flack.py
+++ app/flack.py
@@ -8,6 +8,17 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 app.secret_key = 'adamiscool'
 
+channels = ["TeamFlack"]
+
 @app.route("/", methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    return redirect(url_for('channel',channelname="TeamFlack", channels=channels))
+
+@app.route("/channel/<string:channelname>")
+def channel(channelname):
+    return render_template("index.html",channelname=channelname, channels=channels)
+
+@socketio.on('new channel')
+def newchannel(channelname):
+    channels.append(channelname)
+    emit("update channels", broadcast = True)
--- app/static/index.js
+++ app/static/index.js
@@ -1,24 +1,34 @@
 function changedisplayname() {
   var displayname = prompt("Choose a display name: ");
-  if (displayname == "" || displayname == null) {
+  if (displayname == "") {
     alert("Your display name cannot be blank");
     changedisplayname();
   } else {
   localStorage.setItem("displayname",displayname);
   document.getElementById("displayname").innerHTML = localStorage.getItem("displayname");
-  }
-}
-
-function togglesidenavuserdowntooltip() {
-  var tooltip = document.getElementById("sidenavuserdowntooltip")
-  if (tooltip.style.visiblity == "visible")
+};
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
   if (localStorage.getItem("displayname") == null ||
   localStorage.getItem("displayname") == "") {
     changedisplayname();
   } else {
     document.getElementById("displayname").innerHTML = localStorage.getItem("displayname");
-  }
+  };
+
+  socket.on('connect', () => {
+
+    document.getElementById("channelsplus").onclick = () => {
+      var channelname = prompt("Choose a channel name: ");
+      socket.emit('new channel', channelname);
+    };
+  });
+
+  socket.on('update channels', () => {
+    location.reload();
+  });
+
 });
